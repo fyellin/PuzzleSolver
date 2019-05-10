3779e2d1dabcde92c15c775c698d83791c8c040d
Frank Yellin <fy@fyellin.com>
2019-05-09 20:29:12 -0700

    Save me

---
+++ Listener4542.py
@@ -0,0 +1,287 @@
+import collections
+import re
+from types import CodeType
+from typing import Optional, Tuple, Dict, List, Iterator, NewType, NamedTuple, Sequence, Any, Callable, Iterable, \
+    Pattern, FrozenSet, Set, Mapping
+
+import inflect #type: ignore
+
+eng = inflect.engine()
+
+Location = Tuple[int, int]
+ClueValue = NewType('ClueValue', str)
+
+
+LOCATION_TO_CLUE_DICT: Dict[Location, List[Tuple['Clue', int]]] = collections.defaultdict(list)
+
+
+def create_length_to_integer_dict() -> Tuple[Dict[Tuple[int, int], List[ClueValue]], Dict[ClueValue, ClueValue]]:
+    result: Dict[Tuple[int, int], List[ClueValue]] = collections.defaultdict(list)
+    word_sums = dict()
+    for i in range(1, 1000):
+        clue_value = ClueValue(str(i))
+        word = ''.join(i for i in eng.number_to_words(i) if i.islower())
+        length = len(str(i))
+        word_length = len(word)
+        word_sums[clue_value] = ClueValue(str(sum(ord(c) - ord('a') + 1 for c in set(word))))
+        result[(length, word_length)].append(clue_value)
+    return result, word_sums
+
+
+LENGTHS_TO_INTEGERS, WORD_SUMS = create_length_to_integer_dict()
+
+class Clue(NamedTuple):
+    name: str
+    base_location: Location
+    length: int
+    num_letters: int
+    expression: str
+    compiled_expression: CodeType
+    expression_letters: Set[str]
+    is_across: bool
+
+    def locations(self) -> Iterator[Location]:
+        row, column = self.base_location
+        column_delta, row_delta = (1, 0) if self.is_across else (0, 1)
+        for i in range(self.length):
+            yield row + i * row_delta, column + i * column_delta
+
+    def location(self, i: int) -> Location:
+        row, column = self.base_location
+        column_delta, row_delta = (1, 0) if self.is_across else (0, 1)
+        return row + i * row_delta, column + i * column_delta
+
+    def eval(self, dictionary: Dict[str, int]) -> Optional[ClueValue]:
+        value = eval(self.compiled_expression, None, dictionary)
+        if int(value) != value:
+            return None
+        value = int(value)
+        if value < 1:
+            return None
+        return ClueValue(str(value))
+
+    def generate(self) -> Iterable[ClueValue]:
+        # Find all locations that are the start of a clue: this one or another
+        starts = {index for index, location in enumerate(self.locations())
+                        for (_, other_index) in LOCATION_TO_CLUE_DICT[location]
+                        if other_index == 0 }
+        # Don't allow a zero in any of those locations
+        for value in LENGTHS_TO_INTEGERS[(self.length, self.num_letters)]:
+            if all(value[x] != '0' for x in starts):
+                yield value
+
+    @staticmethod
+    def verify_is_180_symmetric() -> None:
+        max_row = 1 + max(row for (row, _) in LOCATION_TO_CLUE_DICT.keys())
+        max_column = 1 + max(column for (_, column) in LOCATION_TO_CLUE_DICT.keys())
+        for clue in CLUES:
+            row, column = clue.base_location
+            # Find the item(s) in the diagonally opposite location of the same type
+            others = {(x, y) for (x, y) in LOCATION_TO_CLUE_DICT[(max_row - row, max_column - column)]
+                      if x.is_across == clue.is_across}
+            # There better be exactly one, and it better have the same length, and its index must be appropriate
+            assert len(others) == 1
+            clue2, index2 = others.pop()
+            assert clue2.length == clue.length
+            assert index2 == clue.length - 1
+
+    def __hash__(self) -> int:
+        return id(self)
+
+    def __eq__(self, other: Any) -> bool:
+        return self is other
+
+    def __str__(self) -> str:
+        return f'<Clue {self.name} at {self.base_location}/{self.length}>'
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+def make(name: str, expression: str, num_letters: int, length: int, base_location: Location) -> 'Clue':
+    expression_to_compile = expression
+    if '=' in expression:
+        lambda_function = 'lambda x, *y: x if all(z == x for z in y) else -1'
+        expression_as_list = expression_to_compile.replace("=", ",")
+        expression_to_compile = f'({lambda_function})({expression_as_list})'
+    compiled_expression = compile(expression_to_compile, '<string>', 'eval')
+    expression_letters = set(ch for ch in expression if ch.isalpha())
+    result = Clue(name, base_location, length, num_letters, expression, compiled_expression, expression_letters, name.isupper())
+
+    for i, location in enumerate(result.locations()):
+        LOCATION_TO_CLUE_DICT[location].append((result, i))
+    return result
+
+
+
+CLUES = (
+    #     definition        numletters len x  y
+    make('A', 'n',                  6, 2, (1, 1)),
+    make('B', 'e',                 16, 3, (1, 3)),
+    make('C', 'f + k/A = k + d/A', 23, 3, (2, 1)),
+    make('D', 'M - H',             21, 3, (2, 4)),
+    make('E', 'f',                 23, 3, (3, 2)),
+    make('F', 'k - b',             11, 2, (3, 5)),
+    make('G', 'd',                  9, 2, (4, 1)),
+    make('H', 'E',                 24, 3, (4, 3)),
+    make('K', 'B - b = d + G',     25, 3, (5, 1)),
+    make('M', 'G',                 18, 3, (5, 4)),
+    make('N', 'B',                 23, 3, (6, 2)),
+    make('P', 'c',                 10, 2, (6, 5)),
+    make('a', 'C',                 23, 3, (1, 2)),
+    make('b', 'A + G',              9, 2, (1, 3)),
+    make('c', 'E',                 22, 3, (1, 5)),
+    make('d', 'F',                 11, 2, (1, 6)),
+    make('e', '(h -  F) / 2',      23, 3, (2, 1)),
+    make('f', 'H',                 25, 3, (2, 4)),
+    make('g', '(H +  D) / 2',      23, 3, (3, 3)),
+    make('h', 'G',                 11, 3, (3, 6)),
+    make('k', 'H',                 24, 3, (4, 2)),
+    make('m', 'C - b',             22, 3, (4, 5)),
+    make('n', 'G',                 10, 2, (5, 1)),
+    make('p', 'D',                  9, 2, (5, 4)),
+)
+
+
+class Intersection(NamedTuple):
+    this_index: int
+    other_clue: Clue
+    other_index: int
+
+    @staticmethod
+    def maybe_make(this: Clue, other: Clue) -> Optional['Intersection']:
+        if this.is_across == other.is_across:
+            return None
+        this_row, this_column = this.base_location
+        other_row, other_column = other.base_location
+        row_delta, column_delta = other_row - this_row, other_column - this_column
+        # if "this" is an across clue, the clash is at (this_row, other_column).
+        # if "this" is a down clue, the class is at (this_column, other_row).
+        my_index, other_index = (column_delta, -row_delta) if this.is_across else (row_delta, -column_delta)
+        if 0 <= my_index < this.length and 0 <= other_index < other.length:
+            return Intersection(my_index, other, other_index)
+        return None
+
+    def values_match(self, this_value: ClueValue, known_clues: Dict[Clue, ClueValue]) -> bool:
+        return this_value[self.this_index] == known_clues[self.other_clue][self.other_index]
+
+    @staticmethod
+    def get_pattern_maker(clue: Clue, intersections: Sequence['Intersection']) -> \
+            Callable[[Dict[Clue, ClueValue]], Pattern[str]]:
+        temp = ['.'] * (clue.length + 1)
+        temp[-1] = '$'
+        if not intersections:
+            # This is just an optimization.  getter() would work just fine.
+            pattern = re.compile(''.join(temp))
+            return lambda x: pattern
+        else:
+            def getter(known_clues: Dict[Clue, ClueValue]) -> Pattern[str]:
+                result = temp[:]
+                for intersection in intersections:
+                    result[intersection.this_index] = known_clues[intersection.other_clue][intersection.other_index]
+                return re.compile(''.join(result))
+            return getter
+
+
+DEBUG = True
+
+
+class Solver2:
+    count_total: int
+    known_clues: Dict[Clue, ClueValue]
+    base: int
+
+    def __init__(self) -> None:
+        pass
+
+    def solve(self) -> None:
+        self.count_total = 0
+        self.known_clues = {}
+        possible_values = {clue: list(clue.generate()) for clue in CLUES }
+        self.__solve(possible_values)
+
+    def __solve(self, possible_values: Dict[Clue, List[ClueValue]]) -> None:
+        depth = len(self.known_clues)
+        if not possible_values:
+            self._print_board()
+            return
+        clue, values = min(possible_values.items(), key=lambda x: len(x[1]))
+        if not values:
+            if DEBUG or True:
+                print(f'{"|  " * depth}{clue.name} 0/0')
+            return
+        try:
+            known_letters = {x.name for x in self.known_clues}
+            known_letters.add(clue.name)
+            new_expressions = { x for x in CLUES
+                                if x.expression_letters.issubset(known_letters) and clue.name in x.expression_letters }
+            if new_expressions:
+                print(f'{"|  " * depth}{clue.name} CLUES: {[x.name for x in new_expressions]}')
+
+            for i, value in enumerate(values):
+                self.count_total += 1
+                print(f'{"|  " * depth}{clue.name} {i + 1}/{len(values)}: {value} -->')
+                self.known_clues[clue] = value
+
+                next_possible_values = possible_values.copy()
+                if new_expressions:
+                    dictionary = {clue.name: int(value) for clue, value in self.known_clues.items()}
+                    def handle_expression(expression: Clue) -> bool:
+                        evaluation = expression.eval(dictionary)
+                        if expression.name in known_letters:
+                            answer = self.known_clues[expression]
+                            word_sums = WORD_SUMS[answer]
+                            result = (word_sums == evaluation)
+                            print(f'{"|  " * depth}*{expression.name} wc({answer}) = {word_sums} {"=" if result else "!="} {evaluation}')
+                            return result
+                        else:
+                            old_npv = next_possible_values[expression]
+                            new_npv = [t for t in next_possible_values[expression] if WORD_SUMS[t] == evaluation]
+                            print(f'{"|  " * depth}*{expression.name} {old_npv} -> {new_npv}')
+                            next_possible_values[expression] = new_npv
+                            return bool(new_npv)
+
+                    if not all(handle_expression(x) for x in new_expressions):
+                        continue
+
+                def update(this_clue: Clue, this_value: List[ClueValue]) -> List[ClueValue]:
+                    intersection = Intersection.maybe_make(this_clue, clue)
+                    if intersection:
+                        my_index = intersection.this_index
+                        needed_char = value[intersection.other_index]
+                        result = [x for x in this_value if x != value and x[my_index] == needed_char]
+                    else:
+                        result = [x for x in this_value if x != value]
+                    return result
+
+                next_possible_values.pop(clue)
+                for clue2, value2 in next_possible_values.items():
+                    next_possible_values[clue2] = update(clue2, value2)
+                    if value2 != next_possible_values[clue2]:
+                        print(f'{"|  " * depth}+{clue2.name} {value2} -> {next_possible_values[clue2]}')
+
+                self.__solve(next_possible_values)
+
+        finally:
+            self.known_clues.pop(clue, None)
+
+
+    def _print_board(self) -> None:
+        max_row = 1 + max(row for (row, _) in LOCATION_TO_CLUE_DICT.keys())
+        max_column = 1 + max(column for (_, column) in LOCATION_TO_CLUE_DICT.keys())
+        board = [[' ' for _ in range(max_column)] for _ in range(max_row)]
+        for clue, clue_value in self.known_clues.items():
+            for (row, column), letter in zip(clue.locations(), clue_value):
+                board[row - 1][column - 1] = letter
+        print('\n'.join(''.join(bl) for bl in board))
+
+
+def run() -> None:
+    Clue.verify_is_180_symmetric()
+    solver = Solver2()
+    solver.solve()
+    print(solver.count_total)
+
+
+if __name__ == '__main__':
+    foo()
---
+++ magpie2019-05.py
@@ -0,0 +1,373 @@
+import itertools
+import re
+import typing
+from collections import Counter
+from datetime import datetime
+from operator import itemgetter
+from types import CodeType
+from typing import Optional, Tuple, Dict, List, Iterator, NewType, NamedTuple, Set, Sequence, cast, Any, Callable, \
+    Pattern, FrozenSet
+
+DEBUG = False
+
+
+Location = Tuple[int, int]
+ClueValue = NewType('ClueValue', str)
+Letter = NewType('Letter', str)
+
+
+class Clue(NamedTuple):
+    name: str
+    is_across: bool
+    base_location: Location
+    length: int
+    expression: str
+    compiled_expression: CodeType
+
+    @staticmethod
+    def make(name: str, is_across: bool, base_location: Location, length: int, expression: str = '0') -> 'Clue':
+        expression_to_compile = Clue.__fixup_for_compilation(expression)
+        if '=' in expression:
+            lambda_function = 'lambda x, *y: x if all(z == x for z in y) else -1'
+            expression_as_list = expression_to_compile.replace("=", ",")
+            expression_to_compile = f'({lambda_function})({expression_as_list})'
+        compiled_expression = compile(expression_to_compile, f'<Clue {name}>', 'eval')
+        return Clue(name, is_across, base_location, length, expression, compiled_expression)
+
+    def locations(self) -> Iterator[Location]:
+        row, column = self.base_location
+        column_delta, row_delta = (1, 0) if self.is_across else (0, 1)
+        for i in range(self.length):
+            yield row + i * row_delta, column + i * column_delta
+
+    def location(self, i: int) -> Location:
+        row, column = self.base_location
+        column_delta, row_delta = (1, 0) if self.is_across else (0, 1)
+        return row + i * row_delta, column + i * column_delta
+
+    def eval(self, known_letters: Dict[Letter, int]) -> Optional[ClueValue]:
+        value = eval(self.compiled_expression, None, cast(Any, known_letters))
+        if int(value) != value:
+            return None
+        value = int(value)
+        if value < 1:
+            return None
+        return ClueValue(str(value))
+
+    @staticmethod
+    def __fixup_for_compilation(expression: str) -> str:
+        """Convert a magpie expression into a Python expression"""
+        expression = expression.replace("–", "-")  # Magpie use a strange minus sign
+        expression = expression.replace("^", "**")  # Magpie use a strange minus sign
+        for _ in range(2):
+            # ), letter, or digit followed by (, letter, or digit needs an * in between, except when we have
+            # two digits in a row with no space between them.  Note negative lookahead below.
+            expression = re.sub(r'(?!\d\d)([\w)])\s*([(\w])', r'\1*\2', expression)
+        return expression
+
+    def __hash__(self) -> int:
+        return id(self)
+
+    def __eq__(self, other: Any) -> bool:
+        return self is other
+
+    def __str__(self) -> str:
+        return f'<Clue {self.name}>'
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class ClueList:
+    clues: Sequence[Clue]
+    max_row: int
+    max_column: int
+    start_locations: FrozenSet[Location]
+    intersections: FrozenSet[Location]
+
+    def __init__(self, clues: Sequence[Clue]):
+        self.clues = clues
+        final_locations: List[Location] = []
+        start_locations: Set[Location] = set()
+        all_locations: typing.Counter[Location] = Counter()
+        for clue in clues:
+            final_locations.append(clue.location(clue.length - 1))
+            start_locations.add(clue.base_location)
+            all_locations.update(clue.locations())
+        self.start_locations = frozenset(start_locations)
+        self.intersections = frozenset(location for location, count in all_locations.items() if count >= 2)
+        self.max_row = 1 + max(row for (row, _) in final_locations)
+        self.max_column = 1 + max(column for (_, column) in final_locations)
+
+    def __iter__(self) -> Iterator[Clue]:
+        return iter(self.clues)
+
+    @staticmethod
+    def make_clue_list(across: str, down: str, locations: Sequence[Tuple[int, int]]) -> 'ClueList':
+        result: List[Clue] = []
+        for lines, is_across, letter in ((across, True, 'a'), (down, False, 'd')):
+            for line in lines.splitlines():
+                line = line.strip()
+                if not line:
+                    continue
+                match = re.fullmatch(r'(\d+) (.*) \((\d+)\)', line)
+                assert match
+                number = int(match.group(1))
+                location = locations[number - 1]
+                clue = Clue.make(f'{number}{letter}', is_across, location, int(match.group(3)), match.group(2))
+                result.append(clue)
+        return ClueList(result)
+
+    def print_board(self, clue_values: Dict[Clue, ClueValue]) -> None:
+        board = [[' ' for _ in range(self.max_column)] for _ in range(self.max_row)]
+        for clue, clue_value in clue_values.items():
+            for (row, column), letter in zip(clue.locations(), clue_value):
+                board[row - 1][column - 1] = letter
+        print('\n'.join(''.join(bl) for bl in board))
+
+    def verify_is_vertically_symmetric(self) -> None:
+        clue_start_set = self.__make_clue_start_set()
+        for clue in self.clues:
+            # The location in this clue that matches the start of its mirror opposite
+            row, column = clue.location(clue.length - 1 if clue.is_across else 0)
+            # The presumed start location of its mirror opposite
+            row2, column2 = row, self.max_column - column
+            assert ((row2, column2), clue.length, clue.is_across) in clue_start_set
+
+    def verify_is_180_symmetric(self) -> None:
+        clue_start_set = self.__make_clue_start_set()
+        for clue in self.clues:
+            # The location in this clue that matches the start of its symmetric opposite
+            row, column = clue.location(clue.length - 1)
+            # The presumed start location of its symmetric opposite
+            row2, column2 = self.max_column - row, self.max_column - column
+            assert ((row2, column2), clue.length, clue.is_across) in clue_start_set
+
+    def verify_is_four_fold_symmetric(self) -> None:
+        # for each clue, we make sure the next one clockwise is also there.
+        assert self.max_row == self.max_column
+        clue_start_set = self.__make_clue_start_set()
+        for clue in self.clues:
+            # The location in this clue that matches the start of its 90° clockwise rotation
+            row, column = clue.location(0 if clue.is_across else clue.length - 1)
+            # The presumed start location of the clue located 90° clockwise.
+            row2, column2 = column, self.max_column - row
+            # Note that across clues rotate to down clues, and vice versa.
+            assert ((row2, column2), clue.length, not clue.is_across) in clue_start_set
+
+    def __make_clue_start_set(self) -> Set[Tuple[Location, int, bool]]:
+        return {(clue.base_location, clue.length, clue.is_across) for clue in self.clues}
+
+
+class Intersection(NamedTuple):
+    this_index: int
+    other_clue: Clue
+    other_index: int
+
+    @staticmethod
+    def maybe_make(this: Clue, other: Clue) -> Optional['Intersection']:
+        if this.is_across == other.is_across:
+            return None
+        this_row, this_column = this.base_location
+        other_row, other_column = other.base_location
+        row_delta, column_delta = other_row - this_row, other_column - this_column
+        # if "this" is an across clue, the clash is at (this_row, other_column).
+        # if "this" is a down clue, the class is at (this_column, other_row).
+        my_index, other_index = (column_delta, -row_delta) if this.is_across else (row_delta, -column_delta)
+        if 0 <= my_index < this.length and 0 <= other_index < other.length:
+            return Intersection(my_index, other, other_index)
+        return None
+
+    def values_match(self, this_value: ClueValue, known_clues: Dict[Clue, ClueValue]) -> bool:
+        return this_value[self.this_index] == known_clues[self.other_clue][self.other_index]
+
+    @staticmethod
+    def make_pattern_generator(clue: Clue, intersections: Sequence['Intersection'], clue_list: ClueList) -> \
+            Callable[[Dict[Clue, ClueValue]], Pattern[str]]:
+        pattern_list = ['[1-9]' if location in clue_list.start_locations else '.' for location in clue.locations()]
+        pattern_list.append('$')
+
+        if not intersections:
+            pattern = re.compile(''.join(pattern_list))
+            return lambda _: pattern
+
+        # {0}, {1}, etc represent the order the items appear in the  "intersections" argument, not necessarily
+        # the order that they appear in the pattern.  format can handle that.
+        for i, intersection in enumerate(intersections):
+            pattern_list[intersection.this_index] = f'{{{i}:s}}'
+        pattern_format = ''.join(pattern_list)
+
+        def getter(known_clues: Dict[Clue, ClueValue]) -> Pattern[str]:
+            args = (known_clues[intersection.other_clue][intersection.other_index] for intersection in intersections)
+            regexp = pattern_format.format(*args)
+            return re.compile(regexp)
+
+        return getter
+
+
+class SolvingStep(NamedTuple):
+    clue: Clue  # The clue we are solving
+    letters: Sequence[Letter]  # The letters we are assigning a value
+    pattern_maker: Callable[[Dict[Clue, ClueValue]], Pattern[str]]  # a pattern maker
+
+
+class Solver:
+    clue_list: ClueList
+    count_total: int
+    known_letters: Dict[Letter, int]
+    known_clues: Dict[Clue, ClueValue]
+    solving_steps: Sequence[SolvingStep]
+
+    def __init__(self, clue_list: ClueList) -> None:
+        self.clue_list = clue_list
+        self.solving_steps = Solver._get_solving_steps(clue_list)
+
+    @staticmethod
+    def _get_solving_steps(clue_list: ClueList) -> Sequence[SolvingStep]:
+        """Figures out the best order to solve the various clues."""
+        result: List[SolvingStep] = []
+        not_yet_ordered: Dict[Clue, Tuple[Clue, Set[Letter], List[Intersection]]] = {
+            clue: (clue, {Letter(ch) for ch in clue.expression if 'A' <= ch <= 'Z'}, [])
+            for clue in clue_list
+        }
+
+        def evaluator(item: Tuple[Clue, Set[Letter], List[Intersection]]) -> Sequence[int]:
+            # Largest value wins.  Precedence is given to the clue with the least number of unknown variables.
+            # Within that, ties are broken by the one with the most number of intersecting clues.
+            # Within that, ties are broken by the longest clue length, so we create the most intersections
+            clue, unknown_letters, intersections = item
+            return -len(unknown_letters), len(intersections), clue.length
+
+        while not_yet_ordered:
+            clue, unknown_letters, intersections = max(not_yet_ordered.values(), key=evaluator)
+            not_yet_ordered.pop(clue)
+            pattern = Intersection.make_pattern_generator(clue, intersections, clue_list)
+            result.append(SolvingStep(clue, tuple(sorted(unknown_letters)), pattern))
+            for (other_clue, other_unknown_letters, other_intersections) in not_yet_ordered.values():
+                # Update the remaining not_yet_ordered clues, indicating more known letters and updated intersections
+                other_unknown_letters.difference_update(unknown_letters)
+                maybe_clash = Intersection.maybe_make(other_clue, clue)
+                if maybe_clash:
+                    other_intersections.append(maybe_clash)
+        return tuple(result)
+
+    def solve(self) -> None:
+        self.count_total = 0
+        self.known_letters = {}
+        self.known_clues = {}
+        self.__solve(0)
+
+    def __solve(self, current_index: int) -> None:
+        if current_index == len(self.solving_steps):
+            self._print_known_letters()
+            self.clue_list.print_board(self.known_clues)
+            return
+        solving_step = self.solving_steps[current_index]
+        clue = solving_step.clue
+        clue_letters = solving_step.letters
+        current_letter_values = set(self.known_letters.values())
+        pattern = solving_step.pattern_maker(self.known_clues)
+        try:
+            for next_letter_values in itertools.permutations(range(1, 17), len(clue_letters)):
+                if any(v in current_letter_values for v in next_letter_values):
+                    continue
+
+                self.count_total += 1
+                for letter, value in zip(clue_letters, next_letter_values):
+                    self.known_letters[letter] = value
+                clue_value = clue.eval(self.known_letters)
+                if not (clue_value and pattern.match(clue_value)):
+                    continue
+
+                def show_it(info: str) -> None:
+                    if DEBUG:
+                        print(f'{" | " * current_index} {clue.name} {clue_letters} '
+                              f'{next_letter_values} {clue_value} ({clue.length}): {info}')
+
+                self.known_clues[clue] = clue_value
+                show_it('--->')
+                self.__solve(current_index + 1)
+        finally:
+            for letter in clue_letters:
+                self.known_letters.pop(letter, None)
+            self.known_clues.pop(clue, None)
+
+    def _print_known_letters(self) -> None:
+        pairs = [(letter, value) for letter, value in self.known_letters.items()]
+        pairs.sort()
+        print(''.join(f'{letter:<3}' for letter, _ in pairs))
+        print(''.join(f'{value:<3}' for _, value in pairs))
+        pairs.sort(key=itemgetter(1))
+        print(''.join(f'{letter:<3}' for letter, _ in pairs))
+        print(''.join(f'{value:<3}' for _, value in pairs))
+
+
+# noinspection SpellCheckingInspection
+ACROSS = """
+1 ON (2)
+2 S + (A + L)E (2)
+4 ABB (3)
+6 B – A (2)
+7 MARS + ALA (3)
+9 T + REE (2)
+10 ARG + O + T (2)
+12 GL + IB (3)
+14 DEGR/(EES) (2)
+15 BAN – E (3)
+16 PO – SE (2)
+17 AGRAR + IAN (3) 
+19 R + O + LE (2)
+21 SCA – B (2)
+24 SPR + ITE (3)
+27 T + A + LE (2)
+28 UNC + OS (3)
+29 BE + N – S (2)
+30 SAI + L (2)
+"""
+
+# noinspection SpellCheckingInspection
+DOWN = """
+1 A(MB – O)S (3)
+2 G + ALLS (3)
+3 BAA (2)
+4 ARE – A (2)
+5 BR + E + R (2)
+6 BRAG – GART (3)
+8 RAG (2)
+11 PAGEA – NTS (3)
+12 T + O (2)
+13 US (2)
+14 UR (2) 
+15 AGA (2)
+16 ARRESTE/D (3) 
+18 EL + M (2)
+20 DAT + ES (3)
+22 CL + AMP (3)
+23 TI – L + E (2)
+25 EL – E + A (2)
+26 L + AG (2)
+"""
+
+LOCATIONS: Sequence[Location] = (
+             (1, 1), (1, 3), (1, 4), (1, 5), (1, 7),
+             (2, 2), (2, 4), (2, 6), (3, 1), (3, 3),
+             (3, 4), (3, 5), (3, 7), (4, 1), (4, 3),
+             (4, 6), (5, 1), (5, 2), (5, 4), (5, 5),
+             (5, 6), (5, 7), (6, 1), (6, 2), (6, 3),
+             (6, 4), (6, 5), (7, 1), (7, 4), (7, 6))
+
+
+def run() -> None:
+    clue_list = ClueList.make_clue_list(ACROSS, DOWN, LOCATIONS)
+    clue_list.verify_is_four_fold_symmetric()
+
+    time1 = datetime.now()
+    solver = Solver(clue_list)
+    time2 = datetime.now()
+    solver.solve()
+    time3 = datetime.now()
+    print(solver.count_total, time2 - time1, time3 - time2, time3 - time1)
+
+
+if __name__ == '__main__':
+    run()
