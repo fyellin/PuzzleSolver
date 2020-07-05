1728cc50586c25c11d6b65ad484d833fa0f1ee9e
Frank Yellin <fy@fyellin.com>
2020-07-05 16:31:50 -0700

    Slight refactoring

--- sudoku/dancing_sudoku.py
+++ sudoku/dancing_sudoku.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import Tuple, Sequence,  Dict, List
+from typing import Tuple, Sequence, Dict, List, Set
 
 from matplotlib import pyplot as plt
 
@@ -8,6 +8,8 @@ from solver import DancingLinks
 
 class Sudoku:
     initial_grid: Dict[Tuple[int, int], int]
+    constraints: Dict[Tuple[int, int, int], List[str]]
+    optional_constraints: Set[str]
 
     def solve(self, puzzle: str, *,
               king: bool = False, knight: bool = False, adjacent: bool = False,
@@ -16,29 +18,82 @@ class Sudoku:
               evens: Sequence[Tuple[int, int]] = (),
               snakes: Sequence[Tuple[int, int]] = ()) -> None:
 
-        initial_grid = {(row, column): int(letter)
-                               for (row, column), letter in zip(itertools.product(range(1, 10), repeat=2), puzzle)
-                               if '1' <= letter <= '9'}
-        self.initial_grid = initial_grid
+        self.initial_grid = {(row, column): int(letter)
+                             for (row, column), letter in zip(itertools.product(range(1, 10), repeat=2), puzzle)
+                             if '1' <= letter <= '9'}
 
         def row_printer(results: Sequence[Tuple[int, int, int]]) -> None:
-            self.draw_grid(initial_grid, results)
+            self.draw_grid(results)
 
-        constraints = self.__get_default_constraints()
-        optional_constraints = set()
+        self.constraints = self.__get_default_constraints()
+        self.optional_constraints = set()
 
         deletions = {(row, column, valuex)
-                     for (row, column), value in initial_grid.items()
-                     for valuex in range(1, 10) if valuex != value }
+                     for (row, column), value in self.initial_grid.items()
+                     for valuex in range(1, 10) if valuex != value}
 
-        def impossible(triple1: Tuple[int, int, int], triple2: Tuple[int, int, int], name: str = ''):
-            row1, column1, value1 = triple1
-            row2, column2, value2 = triple2
-            constraint = f'{name}:r{row1}c{column1}={value1};r{row2}c{column2}={value2}'
-            optional_constraints.add(constraint)
-            constraints[row1, column1, value1].append(constraint)
-            constraints[row2, column2, value2].append(constraint)
+        self.handle_chess(king, knight, adjacent)
 
+        for thermo_index, thermometer in enumerate(thermometers, start=2):
+            length = len(thermometer)
+            span = 10 - length  # number of values each element in thermometer can have
+            for minimum, (row, column) in enumerate(thermometer, start=1):
+                maximum = minimum + span - 1
+                deletions.update((row, column, value) for value in range(1, 10) if not minimum <= value <= maximum)
+            prefix = f'T{thermo_index}'
+            for (index1, (row1, col1)), (index2, (row2, col2)) in itertools.combinations(enumerate(thermometer), 2):
+                for value1, value2 in itertools.product(range(1, 10), repeat=2):
+                    if value2 < value1 + (index2 - index1):
+                        self.not_both((row1, col1, value1), (row2, col2, value2), prefix)
+
+        if magic:
+            self.handle_magic(deletions)
+
+        if self is None:
+            # This handles the cups, in which certain squares had to differ by 5 or more
+            cup1 = ((1, 2), (2, 2), (3, 3), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4), (8, 3))
+            cup2 = tuple((row, 10 - column) for row, column in cup1)
+            deletions.update((row, column, 5) for cup in (cup1, cup2) for row, column in cup)
+            for cup in (cup1, cup2):
+                for ((row1, col1), (row2, col2)) in zip(cup, cup[1:]):
+                    for value1, value2 in itertools.product(range(1, 10), repeat=2):
+                        if abs(value1 - value2) < 5:
+                            self.impossible((row1, col1, value1), (row2, col2, value2))
+
+        if self is None:
+            # This handled a circle, in which adjacent values had to add to a square or cube, and there had to be one
+            # of every digit in the circle
+            square = ((2, 4), (2, 5), (2, 6), (3, 7), (4, 8), (5, 8), (6, 8), (7, 7),
+                      (8, 6), (8, 5), (8, 4), (7, 3), (6, 2), (5, 2), (4, 2), (3, 3))
+            for ((row1, col1), (row2, col2)) in zip(square, square[1:] + square[:1]):
+                for value1, value2 in itertools.product(range(1, 10), repeat=2):
+                    if value1 + value2 not in (1, 4, 8, 9, 16):
+                        self.impossible((row1, col1, value1), (row2, col2, value2), 'c')
+
+            def row_printer(results: Sequence[Tuple[int, int, int]]) -> None:
+                values_in_circle = {value for (row, column, value) in results if (row, column) in square}
+                if len(values_in_circle) == 9:
+                    self.draw_grid(results)
+
+        for i, snake in enumerate(snakes):
+            for value in range(1, 10):
+                constraint = f"Snake{i}={value}"
+                # If the snake has length 9, each of the constraints must be fulfilled.  Otherwise, they're optional.
+                if len(snake) < 9:
+                    self.optional_constraints.add(constraint)
+                for row, column in snake:
+                    self.constraints[row, column, value].append(constraint)
+
+        if evens:
+            deletions.update((row, column, value) for row, column in self.evens for value in (1, 3, 5, 7, 9))
+
+        for key in deletions:
+            self.constraints.pop(key)
+
+        links = DancingLinks(self.constraints, optional_constraints=self.optional_constraints, row_printer=row_printer)
+        links.solve(debug=1000, recursive=False,)
+
+    def handle_chess(self, king, knight, adjacent):
         for row, column in itertools.product(range(1, 10), repeat=2):
             if knight or king:
                 neighbors = set()
@@ -51,93 +106,44 @@ class Sudoku:
                 for neighbor_row, neighbor_column in neighbors:
                     if (row, column) < (neighbor_row, neighbor_column):
                         for value in range(1, 10):
-                            impossible((row, column, value), (neighbor_row, neighbor_column, value), 'C')
+                            self.not_both((row, column, value), (neighbor_row, neighbor_column, value), 'C')
 
             if adjacent:
                 # For each cell next me me, create an optional constraint such that if I have a specific value,
                 # the neighbor cannot have one more than that value.
                 for neighbor_row, neighbor_column in self.adjacent_move(row, column):
                     for value in range(1, 9):  # Note, we don't need to use value=9.
-                        impossible((row, column, value), (neighbor_row, neighbor_column, value + 1))
+                        self.not_both((row, column, value), (neighbor_row, neighbor_column, value + 1))
 
-        for thermo_index, thermometer in enumerate(thermometers, start=2):
-            length = len(thermometer)
-            span = 10 - length  # number of values each element in thermometer can have
-            for minimum, (row, column) in enumerate(thermometer, start=1):
-                maximum = minimum + span - 1
-                deletions.update((row, column, value) for value in range(1, 10) if not minimum <= value <= maximum)
-            prefix = f'T{thermo_index}'
-            for (index1, (row1, col1)), (index2, (row2, col2)) in itertools.combinations(enumerate(thermometer), 2):
-                for value1, value2 in itertools.product(range(1, 10), repeat=2):
-                    if value2 < value1 + (index2 - index1):
-                        impossible((row1, col1, value1), (row2, col2, value2), prefix)
+    def handle_magic(self, deletions):
+        deltas = ((-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1))
+        values = (2, 9, 4, 3, 8, 1, 6, 7)
+        for center_x, center_y in ((2, 6), (4, 2), (6, 8), (8, 4)):
+            squares = [(center_x + delta_x, center_y + delta_y) for delta_x, delta_y in deltas]
 
-        if magic:
-            deltas = ((-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1))
-            values = (2, 9, 4, 3, 8, 1, 6, 7)
+            # The center must be five, the corners must be even, and the others must be odd.
+            deletions.update((center_x, center_y, value) for value in range(1, 10) if value != 5)
+            deletions.update((row, column, value) for (row, column) in squares[0::2] for value in (1, 3, 5, 7, 9))
+            deletions.update((row, column, value) for (row, column) in squares[1::2] for value in (2, 4, 5, 6, 8))
 
-            for center_x, center_y in ((2, 6), (4, 2), (6, 8), (8, 4)):
-                squares = [(center_x + delta_x, center_y + delta_y) for delta_x, delta_y in deltas]
+            prefix = f'MS{center_x}{center_y}'
+            for (i1, (row1, col1)), (i2, (row2, col2)) in itertools.combinations(enumerate(squares), 2):
+                delta = i2 - i1
+                for value_index in (0, 2, 4, 6):
+                    value1 = values[(value_index + i1) % 8]
+                    value2a = values[(value_index + i1 + delta) % 8]
+                    value2b = values[(value_index + i1 - delta) % 8]
+                    for value2 in range(1, 10):
+                        if value2 != value2a and value2 != value2b:
+                            self.not_both((row1, col1, value1), (row2, col2, value2), prefix)
 
-                # The center must be five, the corners must be even, and the others must be odd.
-                deletions.update((center_x, center_y, value) for value in range(1, 10) if value != 5)
-                deletions.update((row, column, value) for (row, column) in squares[0::2] for value in (1, 3, 5, 7, 9))
-                deletions.update((row, column, value) for (row, column) in squares[1::2] for value in (2, 4, 5, 6, 8))
-
-                for (i1, (row1, col1)), (i2, (row2, col2)) in itertools.combinations(enumerate(squares), 2):
-                    delta = i2 - i1
-                    for value_index in (0, 2, 4, 6):
-                        value1 = values[(value_index + i1) % 8]
-                        value2a = values[(value_index + i1 + delta) % 8]
-                        value2b = values[(value_index + i1 - delta) % 8]
-                        for value2 in range(1, 10):
-                            if value2 != value2a and value2 != value2b:
-                                impossible((row1, col1, value1), (row2, col2, value2), f'MS{center_x}{center_y}')
-
-        if self is None:
-            # This handles the cups, in which certain squares had to differ by 5 or more
-            cup1 = ((1, 2), (2, 2), (3, 3), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4), (8, 3))
-            cup2 = tuple((row, 10 - column) for row, column in cup1)
-            deletions.update((row, column, 5) for cup in (cup1, cup2) for row, column in cup)
-            for cup in (cup1, cup2):
-                for ((row1, col1), (row2, col2)) in zip(cup, cup[1:]):
-                    for value1, value2 in itertools.product(range(1, 10), repeat=2):
-                        if abs(value1 - value2) < 5:
-                            impossible((row1, col1, value1), (row2, col2, value2))
-
-        if self is None:
-            # This handled a circle, in which adjacent values had to add to a square or cube, and there had to be one
-            # of every digit in the circle
-            square = ((2, 4), (2, 5), (2, 6), (3, 7), (4, 8), (5, 8), (6, 8), (7, 7),
-                      (8, 6), (8, 5), (8, 4), (7, 3), (6, 2), (5, 2), (4, 2), (3, 3))
-            for ((row1, col1), (row2, col2)) in zip(square, square[1:] + square[:1]):
-                for value1, value2 in itertools.product(range(1, 10), repeat=2):
-                    if value1 + value2 not in (1, 4, 8, 9, 16):
-                        impossible((row1, col1, value1), (row2, col2, value2), 'c')
-
-            def row_printer(results: Sequence[Tuple[int, int, int]]) -> None:
-                values = {value for (row, column, value) in results if (row, column) in square}
-                if len(values) == 9:
-                    self.draw_grid(initial_grid, results)
-
-        for i, snake in enumerate(snakes):
-            for value in range(1, 10):
-                constraint = f"Snake{i}={value}"
-                # If the snake has length 9, each of the constraints will be fulfilled.  Otherwise, they're optional.
-                if len(snake) < 9:
-                    optional_constraints.add(constraint)
-                for row, column in snake:
-                    constraints[row, column, value].append(constraint)
-
-        if evens:
-            deletions.update((row, column, value) for row, column in self.evens for value in (1, 3, 5, 7, 9))
-
-        for key in deletions:
-            constraints.pop(key)
-
-        links = DancingLinks(constraints, optional_constraints=optional_constraints, row_printer=row_printer)
-        links.solve(debug=1000, recursive=False,)
-
+    def not_both(self, triple1: Tuple[int, int, int], triple2: Tuple[int, int, int], name: str = ''):
+        row1, column1, value1 = triple1
+        row2, column2, value2 = triple2
+        constraint = f'{name}:r{row1}c{column1}={value1};r{row2}c{column2}={value2}'
+        self.optional_constraints.add(constraint)
+        self.constraints[triple1].append(constraint)
+        self.constraints[triple2].append(constraint)
 
     @staticmethod
     def knights_move(row: int, column: int) -> Sequence[Tuple[int, int]]:
@@ -159,8 +165,7 @@ class Sudoku:
                 for dr, dc in ((dx, 0), (0, dx))
                 if 1 <= row + dr <= 9 and 1 <= column + dc <= 9]
 
-    @staticmethod
-    def draw_grid(initial_grid, results: Sequence[Tuple[int, int, int]]) -> None:
+    def draw_grid(self, results: Sequence[Tuple[int, int, int]]) -> None:
         figure, axes = plt.subplots(1, 1, figsize=(4, 4), dpi=100)
 
         # Set (1,1) as the top-left corner, and (max_column, max_row) as the bottom right.
@@ -178,7 +183,7 @@ class Sudoku:
         given = dict(fontsize=13, color='black', weight='heavy')
         found = dict(fontsize=12, color='blue', weight='normal')
         for row, column, value in results:
-            args = given if (row, column) in initial_grid else found
+            args = given if (row, column) in self.initial_grid else found
             axes.text(column + .5, row + .5, str(value),
                       verticalalignment='center', horizontalalignment='center', **args)
         plt.show()
@@ -188,10 +193,11 @@ class Sudoku:
         def box(row: int, column: int) -> int:
             return row - (row - 1) % 3 + (column - 1) // 3
 
-        return {(row, column, value):
-                    [f"V{row}{column}", f"R{row}={value}", f"C{column}={value}", f"B{box(row, column)}={value}"]
+        return {(row, column, value): [f"V{row}{column}", f"R{row}={value}",
+                                       f"C{column}={value}", f"B{box(row, column)}={value}"]
                 for row, value, column in itertools.product(range(1, 10), repeat=3)
                 }
+
 
 def merge(p1: str, p2: str):
     assert len(p1) == len(p2) == 81
@@ -202,8 +208,8 @@ def merge(p1: str, p2: str):
 
 def main() -> None:
     # XUZZ = "123456789123456789123456789123456789123456789123456789123456789123456789123456789"
-    PUZZLE = ".................1.......................................................6......."
-    Sudoku().solve(PUZZLE, magic=True)
+    puzzle = ".................1.......................................................6......."
+    Sudoku().solve(puzzle, magic=True)
 
     # PREVIO = "..7......3.....5......................3..8............15.............9....9......"
     # PUZZLE = "......3...1...............72.........................2..................8........"
@@ -211,6 +217,7 @@ def main() -> None:
     # thermometers = [[(row, column) for row in (9, 8, 7, 6, 5, 4)] for column in (2, 4, 6, 8)]
     # snake = ((1, 4), (1, 6), (2, 1), (2, 9), (3, 3), (3, 5), (3, 7), (4, 2), (4, 8))
     # Sudoku().solve(merge(PUZZLE, PREVIO), thermometers=thermometers, snake=snake)
+
 
 if __name__ == '__main__':
     main()
