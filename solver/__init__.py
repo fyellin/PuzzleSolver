19c3ad448f7aa82db234ab73968ca6ce0ae84828
fyellin <fy@fyellin.com>
2026-02-14 18:42:00 -0800

    Remove encoding.  Small updates (#2)

--- solver/__init__.py
+++ solver/__init__.py
@@ -5,13 +5,10 @@ from .clue import Clue, ClueValueGenerator
 from .clue_types import Location, ClueValue, Letter
 from .clues import Clues
 from .constraint_solver import ConstraintSolver
-from .dancing_links import DancingLinks
+from .dancing_links import DancingLinks, DLConstraint
 from .equation_solver import EquationSolver
 from .multi_equation_solver import MultiEquationSolver
 from .evaluator import Evaluator
-from .generators import ClueValueGenerator
 from .intersection import Intersection
 from .playfair import PlayfairSolver
-
-from .dancing_links import DancingLinks
-from .dancing_links_helper import Orderer, Encoder
+from .dancing_links_helper import Orderer
